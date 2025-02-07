924e115a6eaf7fcb54aaf3ece5fa2f6179490415
ArsenalGenieJames <144416574+ArsenalGenieJames@users.noreply.github.com>
2025-02-07 09:56:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -98,9 +98,3 @@ function updateDateTime() {
 
 setInterval(updateDateTime, 1000);
 updateDateTime();
-
-//lockinspectelement
-
-
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.addEventListener('keydown', function (event) { if (event.ctrlKey && (event.key === 'u' || event.key === 's' || event.key === 'a' || event.key === 'c' || event.key === 'x')) { event.preventDefault(); } if (event.key === 'F12' || event.key === 'I' && event.shiftKey && event.ctrlKey) { event.preventDefault(); } });
